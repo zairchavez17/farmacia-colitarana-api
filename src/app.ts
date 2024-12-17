229281882e9bd5e91ef076f16c47a4966bb22737
zairchavez17 <ac3989665@gmail.com>
2024-12-17 18:02:46 -0500

    metodo saludar

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 
 const app: Application = express();
+
+app.get('/saludar', (req: Request, res: Response) => { 
+    console.log('Hola estoy saludando'); 
+    res.json({ nombre: 'Diego' });
+});
 
 app.use(morgan('dev'));
 
