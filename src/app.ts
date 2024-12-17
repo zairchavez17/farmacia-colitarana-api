ea5809a6c36dbf7b69232bef790b301a2f7cda1d
zairchavez17 <ac3989665@gmail.com>
2024-12-17 18:24:24 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,7 @@ import morgan from 'morgan';
 const app: Application = express();
 
 app.get('/saludar', (req: Request, res: Response) => { 
-    console.log('Hola estoy saludando'); 
-    res.json({ nombre: 'Diego Fernando', apellido: 'Baes' });
+     res.json({ nombre: 'Diego Fernando', apellido: 'Baes' });
 });
 
 app.use(morgan('dev'));
