af3222f854ea4a9fde263d69e4ffd2bd20eb17f0
zairchavez17 <ac3989665@gmail.com>
2024-12-18 21:24:00 -0500

    agregado typeorm

---
+++ src/config/db.config.ts
@@ -0,0 +1,11 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'BD_COLITARANA',
+    entities: [],
+});
